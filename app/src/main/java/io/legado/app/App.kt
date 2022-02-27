99cbe3326e212270786da97c6f5f6c60cd84d7d1
amos <414983570@qq.com>
2022-02-27 17:00:50 +0800

    fix compile error

--- app/src/main/java/io/legado/app/App.kt
+++ app/src/main/java/io/legado/app/App.kt
@@ -19,6 +19,9 @@ import io.legado.app.help.ThemeConfig.applyDayNight
 import io.legado.app.help.http.cronet.CronetLoader
 import io.legado.app.utils.defaultSharedPreferences
 import splitties.systemservices.notificationManager
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 class App : MultiDexApplication() {
 
